f189bc37ea4c3f81209796a3e5d65deb9d323ca9
iscfen <1412591585@qq.com>
2024-08-16 21:25:02 +1000

    changes for main js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App.vue'
+
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
-import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/main.css';
+
+
 
 
 const app = createApp(App);
