43bd93f51ea135ee780494242beae9c0dfc73660
iscfen <1412591585@qq.com>
2024-09-10 20:59:32 +1000

    function complete for w7

--- src/main.js
+++ src/main.js
@@ -7,8 +7,29 @@ import router from "./router";
 import PrimeVue from "primevue/config";
 import Aura from "@primevue/themes/aura";
 
-const app = createApp(App);
-app.use(PrimeVue, { theme: { preset: Aura } });
-app.use(router);
+// Initialize Vue app
+const vueApp = createApp(App);
+vueApp.use(PrimeVue, { theme: { preset: Aura } });
+vueApp.use(router);
 
-app.mount("#app");
+// Import the functions you need from the Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_rsAfOlylzLSqSfBBLxCPC6iv5ALFdAE",
+  authDomain: "week7-jiayuan-6b508.firebaseapp.com",
+  projectId: "week7-jiayuan-6b508",
+  storageBucket: "week7-jiayuan-6b508.appspot.com",
+  messagingSenderId: "697403837256",
+  appId: "1:697403837256:web:66b381bc0d37a65d16817d",
+  measurementId: "G-4X6ZF6NJR0"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+
+// Mount the Vue app
+vueApp.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import LoginView from "../views/LoginView.vue";
 import AccessDeniedView from "../views/AccessDeniedView.vue";
+import FirebaseSigninView from "@/views/FirebaseSigninView.vue";
+import FirebaseRegisterView from "@/views/FirebaseRegisterView.vue";
+
 
 const isAuthenticated = () =>
   localStorage.getItem("isAuthenticated") === "true";
@@ -17,6 +20,8 @@ const routes = [
   },
   { path: "/login", name: "Login", component: LoginView },
   { path: "/access-denied", name: "AccessDenied", component: AccessDeniedView },
+  { path: "/FireLogin", name: "FireLogin", component: FirebaseSigninView },
+  { path: "/FireRegister", name: "FireRegister", component: FirebaseRegisterView },
 ];
 
 const router = createRouter({
