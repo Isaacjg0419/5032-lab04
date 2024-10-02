9c004931d9f1857b8124291db419d929c0a4a83f
iscfen <1412591585@qq.com>
2024-10-03 00:09:49 +1000

    init new cloud functions

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,7 @@ const cors = require("cors")({ origin: true });
 
 admin.initializeApp();
 
+// Function to count the number of books
 exports.countBooks = onRequest((req, res) => {
   cors(req, res, async () => {
     try {
@@ -15,6 +16,26 @@ exports.countBooks = onRequest((req, res) => {
     } catch (error) {
       console.error("Error counting books:", error.message);
       res.status(500).send("Error counting books");
+    }
+  });
+});
+
+// Function to get all books
+exports.getAllBooks = onRequest((req, res) => {
+  cors(req, res, async () => {
+    try {
+      const booksCollection = admin.firestore().collection("books");
+      const snapshot = await booksCollection.get();
+
+      const books = [];
+      snapshot.forEach(doc => {
+        books.push({ id: doc.id, ...doc.data() });
+      });
+
+      res.status(200).send(books);
+    } catch (error) {
+      console.error("Error fetching books:", error.message);
+      res.status(500).send("Error fetching books");
     }
   });
 });
--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -1,81 +1,134 @@
 <template>
     <div class="container">
-        <div class="header">
-            <h1>WEATHER APP</h1>
-            <div class="search-bar">
-                <input type="text" v-model="city" placeholder="Enter city name" class="search-input" />
-                <button @click="searchByCity" class="search-button">Search</button>
-            </div>
+      <div class="header">
+        <h1>Weather App</h1>
+        <div class="search-bar">
+          <input
+            type="text"
+            v-model="city"
+            placeholder="Enter city name"
+            class="search-input"
+          />
+          <button @click="searchByCity" class="search-button">Search</button>
         </div>
-
-        <main>
-            <div v-if="weatherData">
-                <h2>
-                    {{ weatherData.name }}, {{ weatherData.sys.country }}
-                </h2>
-                <div>
-                    <img :src="iconUrl" alt="Weather Icon" />
-                    <p>{{ temperature }} °C</p>
-                </div>
-                <span>{{ weatherData.weather[0].description }}</span>
-            </div>
-        </main>
+        <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+      </div>
+  
+      <main>
+        <div v-if="weatherData">
+          <h2>
+            {{ weatherData.name }}, {{ weatherData.sys.country }}
+          </h2>
+          <div>
+            <img :src="iconUrl" alt="Weather Icon" />
+            <p>{{ temperature }} °C</p>
+          </div>
+          <span>{{ weatherData.weather[0].description }}</span>
+        </div>
+      </main>
     </div>
-</template>
-
-<script>
-import axios from "axios";
-
-const apikey = "005d4f65e27d657fa79f18a1e68feebc"; 
-
-export default {
-  name: "App",
-  data() {
-    return {
-      city: "", 
-      weatherData: null,
-    };
-  },
-  computed: {
-    temperature() {
-      return this.weatherData ? Math.floor(this.weatherData.main.temp - 273.15) : null;
+  </template>
+  
+  <script>
+  import axios from "axios";
+  
+  const apikey = "005d4f65e27d657fa79f18a1e68feebc";
+  
+  export default {
+    name: "App",
+    data() {
+      return {
+        city: "",
+        weatherData: null,
+        errorMessage: null,
+      };
     },
-
-    iconUrl() {
-      return this.weatherData
-        ? `http://openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
-        : null;
+    computed: {
+      temperature() {
+        return this.weatherData ? Math.floor(this.weatherData.main.temp - 273.15) : null;
+      },
+      iconUrl() {
+        return this.weatherData
+          ? `http://openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
+          : null;
+      },
     },
-  },
-  mounted() {
-    this.fetchCurrentLocationWeather(); 
-  },
-  methods: {
-    async fetchCurrentLocationWeather() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(async (position) => {
-          const { latitude, longitude } = position.coords;
-          const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+    mounted() {
+      this.fetchCurrentLocationWeather();
+    },
+    methods: {
+      async fetchCurrentLocationWeather() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            async (position) => {
+              const { latitude, longitude } = position.coords;
+              const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+              await this.fetchWeatherData(url);
+            },
+            (error) => {
+              this.errorMessage = "Unable to get your location.";
+              console.error("Geolocation error:", error);
+            }
+          );
+        } else {
+          this.errorMessage = "This browser does not support geolocation.";
+        }
+      },
+      async fetchWeatherData(url) {
+        try {
+          const response = await axios.get(url);
+          this.weatherData = response.data; 
+          this.errorMessage = null; 
+        } catch (error) {
+          this.weatherData = null; 
+          this.errorMessage = "Error fetching weather data, please try again."; 
+          console.error("Error fetching weather data:", error);
+        }
+      },
+      async searchByCity() {
+        if (this.city.trim()) {
+          const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`;
           await this.fetchWeatherData(url);
-        });
-      }
+        } else {
+          this.errorMessage = "Please enter a valid city name."; 
+        }
+      },
     },
-    // 根据提供的API URL获取天气数据
-    async fetchWeatherData(url) {
-      try {
-        const response = await axios.get(url);
-        this.weatherData = response.data; // 将返回的数据存储在 `weatherData` 中
-      } catch (error) {
-        console.error("获取天气数据时出错：", error);
-      }
-    },
-    // 根据用户输入的城市名称搜索天气
-    async searchByCity() {
-      if (this.city) {
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`;
-        await this.fetchWeatherData(url);
-      }
-    },
-  },
-};
-</script>
+  };
+  </script>
+  
+  <style>
+  .container {
+    width: 80%;
+    margin: auto;
+    text-align: center;
+  }
+  
+  .header {
+    margin-bottom: 20px;
+  }
+  
+  .search-bar {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+  }
+  
+  .search-input {
+    padding: 10px;
+    font-size: 16px;
+    width: 200px;
+    margin-right: 10px;
+  }
+  
+  .search-button {
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+  
+  .error-message {
+    color: red;
+    margin-top: 10px;
+  }
+  </style>
+  
